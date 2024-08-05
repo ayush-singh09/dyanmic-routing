e9998ebc84e28731985e0bc1dbeaf729e9dd10ea
ayush-singh09 <insaneraj09@gmail.com>
2024-08-05 21:25:08 +0530

    Engine Bhai ki baat pe

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,48 +1,66 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
 
 function User() {
+  const [users, setUsers] = useState([]);
+  const [filtered, setFiltered] = useState([])
+
   const navigate = useNavigate();
 
-  const users = [
-    {
-      id: "1",
-      username:"john-doe",
-      name: "John Doe",
-      age: "25",
-    },
-    {
-      id: "2",
-      username:"bob-smith",
-      name: "Bob Smith",
-      age: "35",
-    },
-    {
-      id: "3",
-      username:"alena-johnson",
-      name: "Alena Johnson",
-      age: "34",
-    },
-  ];    
+  const getData = async () => {
+    const data = await fetch("https://fakestoreapi.in/api/users");
+    const user = await data.json();
+    setUsers(user.users);
+    setFiltered(user.users);
+  };
+
+  const handleSearch = (search) => {
+    const newData = users.filter((prev) => {
+      return prev.name.firstname.toLowerCase().includes(search.toLowerCase());
+    })
+    setFiltered(newData)
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   return (
-    <div className="flex bg-zinc-800 h-[92%] p-10">
+    <div className="flex bg-zinc-800 min-h-[92%] p-10">
       <div className="flex flex-col gap-3">
         <h1 className="font-bold text-3xl josefin bg-red-300 h-fit w-fit p-2 mb-5">
           User Page
         </h1>
 
-        {users.map((user, index) => {
-          return (
-            <button
-              onClick={() => navigate(`/user/${user.username}`)}
-              key={index}
-              className="bg-yellow-300 text-black p-3"
-            >
-              {user.name}
-            </button>
-          );
-        })}
+        <div className="flex gap-3">
+          <input
+            onChange={(e) => {
+              handleSearch(e.target.value);
+              console.log(e.target.value);
+            }}
+            className="bg-transparent border rounded p-1 px-2 outline-none"
+            placeholder="Search Here"
+            type="text"
+          />
+        </div>
+
+        <div className="flex gap-5">
+          <div className="h-[300px] w-[200px] flex flex-col overflow-y-scroll gap-2 bg-purple-400 p-2">
+            {filtered?.map((user, index) => {
+              return (
+                <button
+                  onClick={() => navigate(`/user/${user.username}`)}
+                  key={index}
+                  className="bg-yellow-300 text-black p-3"
+                >
+                  {user.name.firstname}
+                </button>
+              );
+            })}
+          </div>
+
+          <Outlet />
+        </div>
       </div>
     </div>
   );
--- src/pages/UserInfo.jsx
+++ src/pages/UserInfo.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 function UserInfo() {
+    const navigate = useNavigate()
+  const { username } = useParams();
 
-    const { username } = useParams();
-
-    return <div className="flex bg-zinc-800 h-[92%] p-10">{ username }</div>;
+    return (
+      <div className="flex">
+        <div className="flex flex-col gap-3">
+          <h1 className="text-3xl font-bold">User Details</h1>
+          <h1 className="font-bold">
+            Hii, <span className='text-red-400'>{username}</span>
+          </h1>
+          <button
+            onClick={() => navigate(-1)}
+            className="bg-red-400 h-fit w-fit p-1 px-2"
+          >
+            Go Back
+          </button>
+        </div>
+      </div>
+    );
 }
 
 export default UserInfo
